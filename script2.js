61b235b1e640db748b5966404e401535824c5485
adastr96 <adam19960@op.pl>
2019-03-08 18:07:03 +0100

    nav bar

--- script2.js
+++ script2.js
@@ -1,9 +1,8 @@
 console.log("script2")
 
 //let nav = document.getElementById()
-document.getElementById("nav").innerHTML =  '<ul class="nav col-9">' +
+document.getElementById("nav").innerHTML =
 '<li class="nav-item"><a class="nav-link active" href="index.html">Home</a></li>'+
 '<li class="nav-item"><a class="nav-link" href="#">Blog</a></li>'+
 '<li class="nav-item"><a class="nav-link" href="#">Quiz</a></li>'+
-'<li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>'+
-'</ul> '; 
+'<li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>'; 
