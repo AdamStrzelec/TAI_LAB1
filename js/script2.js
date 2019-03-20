75e52f42a75d24ebd1d1b6459421d693972efc9f
adastr96 <adam19960@op.pl>
2019-03-20 12:14:52 +0100

    quiz done

--- js/script2.js
+++ js/script2.js
@@ -4,5 +4,5 @@ console.log("script2")
 document.getElementById("nav").innerHTML =
 '<li class="nav-item"><a class="nav-link active" href="index.html">Home</a></li>'+
 '<li class="nav-item"><a class="nav-link" href="#">Blog</a></li>'+
-'<li class="nav-item"><a class="nav-link" href="#">Quiz</a></li>'+
+'<li class="nav-item"><a class="nav-link" href="quiz.html">Quiz</a></li>'+
 '<li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>'; 
